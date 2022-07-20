b56a5f8f0138738a0956e9791bf57497df216c45
ahsan <makabir98@yahoo.com>
2022-07-20 15:46:25 +0600

    dfd

---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
+
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+    }, []);
+    return (
+        <div>
+            <h1>Visiting every countries of the world</h1>
+            <h3>Available countries : {countries.length}</h3>
+            {
+                countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>This is header</h1>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Shop</a>
+                <a href="">About Us</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
